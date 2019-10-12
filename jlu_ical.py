2a35c129edea787ba62f8407dbc064285e0169f9
farthing <mhel.farthing@outlook.com>
2019-10-12 11:47:43 +0800

    fix unkown location class bug and now you can change the start date

--- jlu_ical.py
+++ jlu_ical.py
@@ -6,15 +6,7 @@ from datetime import datetime
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
-def upset(string):
-    #string = '线性代数A◇5,6节{第1-16周}◇前卫-经信教学楼#F区第二阶梯◇宋东哲'
-    subject = re.search(r'^.{1,15}◇' , string).group()[0:-1]
-    time = re.search(r'◇.{1,20}◇' , string).group()[1:-1]
-    building = re.search(r'◇.{1,20}#' , string).group()[1:-1]
-    location = re.search(r'#.{1,20}◇' , string).group()[1:-1]
-    teacher = re.search(r'◇.{1,5}$' , string).group()[1:]
-
-    return [subject,teacher,time,building,location]
+start_monday = date(2019,8,26)
 
 def read_excel(day):
     xlsfile = r'学生课程表.xls'
@@ -23,38 +15,47 @@ def read_excel(day):
     sheet_name=book.sheet_names()[0]
     sheet1=book.sheet_by_name(sheet_name)  #通过sheet名字来获取，当然如果你知道sheet名字了可以直接指定  
 
-    day_data = sheet1.col_values(day+2)[3:-1]   #获得第1行的数据列表  
+    day_data = sheet1.col_values(day+2)[3:-1]   #获得从第四行开始直到结束的数据列表  
+
     return day_data
 
+class Course:
+    def __init__(self,string):
+        self.string = string
+
+        self.subject = re.search(r'^(.*?)◇' , string).group()[0:-1]
+        self.time_location = re.search(r'◇(.*)◇' , string).group()[1:-1].split("◇")
+        self.time = self.time_location[0]
+        
+        try:
+            self.building = self.time_location[1].split("#")[0]
+            self.location = self.time_location[1].split("#")[1]
+        except IndexError:      #This means the file not specify the location or building,the common situation is "地点待定".
+            self.building = self.location = self.time_location[1]
+        
+        self.teacher = re.search(r'◇.(.*?)$' , string).group()[1:]
+        self.course_str = [self.subject,self.teacher,self.time,self.building,self.location]
+
 def main():
-    i = 0
     dayclasses = []
-    while (i < 10):
-        if(i != 1 and i != 4 and i != 6):       #JLU class excel exists null col
-            data = read_excel(i)
-
-            n = 1
-
-            while(n < 6):
-                if data[n] != "":
-                    if(re.search(r'\n',data[n]) != None):
-                        interval = data[n].split('\n')
-                        for inter_week in interval:
-                            data_upset = upset(inter_week)
-                            data_upset.append(data[0])
-                            dayclasses.append(data_upset)
+    for index in range(10):
+        if(index != 1 and index != 4 and index != 6):       #JLU class excel exists null col ->col D,G,I
+            data = read_excel(index)
+            for course_no in range(1,5):
+                if data[course_no] != "":
+                    if(re.search(r'\n',data[course_no]) != None):   #This means there are serval courses in the same time at different week.
+                        same_time_course = data[course_no].split('\n')
+                        for course in same_time_course:
+                            each_course = Course(course)
+                            dayclasses.append(each_course.course_str+[data[0]])
 
                     else:
-                        data_upset = upset(data[n])
-                        data_upset.append(data[0])
-                        dayclasses.append(data_upset)
-                n= n + 1
-            write_ics(dayclasses)
-        i =i+1
-        
+                        each_course = Course(data[course_no])
+                        dayclasses.append(each_course.course_str+[data[0]])
+        write_ics(dayclasses)
+
 
 def write_ics(allclasses):
-
 
     cal = Calendar()
     cal['version'] = '2.0'
@@ -69,19 +70,19 @@ def write_ics(allclasses):
                 7: relativedelta(hours=15,minutes=30),8:relativedelta(hours=16,minutes=25),
                 9: relativedelta(hours=18, minutes=30),10:relativedelta(hours=19,minutes=25),
                 11:relativedelta(hours=20,minutes=20)}
-    start_monday = date(2018,3,5)
+
 ##############################################################
 
-
-    
     #string example= ['马克思主义基本原理概论', '孙慧', '9,10,11节{第1-14周}', '前卫-经信教学楼', 'F区第一阶梯']
 
     for string in allclasses:
         day = string[-1]
         
-
-        info_day = re.search(r'^.{1,15}节',string[2]).group()[0:-1].split(',')
-        info_week = re.search(r'第(\d+)-(\d+)周',string[2]).group()[1:-1].split('-')
+        try:
+            info_day = re.search(r'^.{1,15}节',string[2]).group()[0:-1].split(',')
+            info_week = re.search(r'第(\d+)-(\d+)周',string[2]).group()[1:-1].split('-')
+        except:
+            print(string)
 
         dtstart_date = start_monday + relativedelta(weeks=(int(info_week[0]) - 1)) + relativedelta(
             days= (dict_week[day]))
@@ -109,7 +110,6 @@ def write_ics(allclasses):
             event.add('rrule',
                         {'freq': 'weekly', 'interval': interval,
                          'count': int(info_week[1]) - int(info_week[0]) + 1})
-
 
             event.add('location', string[4]+string[3])
             cal.add_component(event)
